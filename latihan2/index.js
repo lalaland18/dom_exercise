b809b244994c34c1fb831d661c3bd731f34b3f57
luthfiyyah iffah adella <luthfiyyah.iffah.adella18@gmail.com>
2024-10-25 13:13:41 +0700

    ubah displaynya

---
+++ latihan2/index.js
@@ -0,0 +1,8 @@
+// mengambil elemen
+const klikButton = document.getElementById("klik")
+const newList = document.getElementById("list")
+
+// membuat elemen baru untuk tombol
+klikButton.addEventListener("click", function(){
+   
+})
